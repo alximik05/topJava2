e23b13179f4eedb3943adbc9248af84d4fb0cf6e
stas.grishaev <oalximiko@gmail.com>
2015-03-15 17:49:18 +0300

    teest3

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -17,8 +17,8 @@ public class UserMeal extends BaseEntity {
 
     private User user;
 
-    public UserMeal() {
-    }
+//    public UserMeal() {
+//    }
 
     public UserMeal(Integer id, LocalDateTime dateTime, String description, int calories) {
         super(id);
